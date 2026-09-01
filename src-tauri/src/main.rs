c7a49aa1edb8a5256b18a0bb1834ebf12768566a
bryanhu <hushao599@gmail.com>
2026-09-01 11:17:13 +0800

    fix(windows): 消除 Codex 后代进程终端闪窗

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,7 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(windows, windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(windows)]
+    windows_process_platform::initialize_hidden_console();
     codeagent_lib::run();
 }
